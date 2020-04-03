e11457fe5c23815a44401db268813c63d474b50e
MyHiHi <909535692@qq.com>
2020-04-03 10:49:46 +0800

    二维整数数组的上下左右四个方向改为新的颜色值.cpp

--- c++/test1.cpp
+++ c++/test1.cpp
@@ -4,30 +4,6 @@ using namespace std;
 int main()
 {
   vector<int> nums;
-  int num;
-  while (getchar() != ']')
-  {
-    cin >> num;
-    nums.push_back(num);
-  }
-  int size = nums.size();
-  int i = 0, a = nums[0];
-  while (1)
-  {
-    i += a;
-    cout << "*****: " << i << " " << a << endl;
-    if (i < 0 || i >= size)
-    {
-      cout << "true";
-      return 0;
-    }
-    if (nums[i] == 0)
-    {
-      cout << "false";
-      return 0;
-    }
-    a = nums[i];
-    nums[i] = 0;
-  }
+  vector<vector<int>> me={nums()};
   return 0;
 }
