e99d57170cd612998c980a9f86ceda52ce0ad29d
MyHiHi <909535692@qq.com>
2020-04-19 19:20:25 +0800

    大整数相加-爬楼梯.cpp

--- leetcode+牛客/test.cpp
+++ leetcode+牛客/test.cpp
@@ -1,45 +1,79 @@
-/*
-@File    :   test.cpp
-@Time    :   2020/04/06 13:24:47
-@Author  :   Zhang tao 
-@Version :   1.0
-@Desc    :   test.cpp
-*/
-/*
-*/
 #include <iostream>
 #include <string>
 using namespace std;
+// string strAdd(string a, string b)
+// {
+//   int carry = 0, i = a.size() - 1, j = b.size() - 1;
+//   string ans = "";
+//   while (i >= 0 || j >= 0)
+//   {
+//     int p1 = i >= 0 ? a[i--] - '0' : 0, p2 = j >= 0 ? b[j--] - '0' : 0;
+//     int v = (p1 + p2 + carry);
+//     carry =v/ 10, ans = to_string(v % 10) + ans;
+//   }
+//   return carry == 1 ? "1" + ans : ans;
+// }
+
+string strAdd(string a, string b)
+{
+  int carry = 0, i = a.size() - 1, j = b.size() - 1, p1 = 0, p2 = 0, v = 0;
+  string ans = "";
+  while (i >= 0 || j >= 0)
+  {
+    p1 = (i >= 0) ? a[i--] - '0' : 0, p2 = (j >= 0) ? b[j--] - '0' : 0;
+    v = (p1 + p2 + carry);
+    carry = v / 10, ans = to_string(v % 10) + ans;
+  }
+  return carry == 1 ? "1" + ans : ans;
+}
+
+
+/***
+ * #include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+string strAdd(string a, string b)
+{
+  int carry = 0, i = a.size() - 1, j = b.size() - 1, p1 = 0, p2 = 0, v = 0;
+  string ans = "";
+  while (i >= 0 || j >= 0)
+  {
+    if (i<0){
+        ans=to_string(b[j--]-'0'+carry)+ans,carry=0;
+    }
+     else  if (j<0){
+        ans=to_string(a[i--]-'0'+carry)+ans,carry=0;
+    }else{
+         int v=a[i--]-'0'+b[j--]-'0'+carry;
+         carry=v/10,ans=to_string(v%10)+ans;
+     }
+  }
+  return carry == 1 ? "1" + ans : ans;
+}
+int main(){
+    int n;
+    cin>>n;
+    string *dp=new string[n+1];
+    dp[0]="1",dp[1]="1",dp[2]="2";
+    for (int t=3;t<n+1;t++){
+        dp[t]=strAdd(dp[t-1],dp[t-2]);
+     
+    }
+    cout<<dp[n];
+}
+ * /
 int main()
 {
-  int size;
-  string s;
-  cin >> size;
-  cin.ignore();
-  getline(cin, s);
-  cout << "s: " << s << endl;
-  int k = size / 3;
-  int t = 0, step = 0, mid = 2 * (k - step) - 1;
-  while (t < size)
-  {
-    // cout << "step: " << step << " " << k << endl;
-    if (step < k)
-    {
-      for (int i = 0; i < step; i++)
-        cout << " ";
-      cout << s[t++];
-      for (int i = 0; i < mid; i++)
-        cout << " ";
-      cout << s[t++] << endl;
-      mid -= 2;
-      step++;
-    }
-    else
-    {
-      for (int i = 0; i < step; i++)
-        cout << " ";
-      cout << s[t++] << endl;
-    }
-  }
-  return 0;
+  // int n;
+  // cin >> n;
+  // string *dp = new string[n + 1];
+  // dp[0] = "0", dp[1] = "1", dp[2] = "2";
+  // for (int t = 3; t < n + 1; t++)
+  // {
+  //   dp[t] = strAdd(dp[t - 1], dp[t - 2]);
+  //   cout << "# " << dp[t] << endl;
+  // }
+  cout << strAdd("123123214321432423423", "783442131312312312312323122") << endl;
+  // cout << dp[n];
 }
