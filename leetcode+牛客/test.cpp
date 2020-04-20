43406187f58d62d181f082252c28b40cade238fb
MyHiHi <909535692@qq.com>
2020-04-20 11:26:24 +0800

    1006. 算术运算顺序-笨阶乘.cpp

--- leetcode+牛客/test.cpp
+++ leetcode+牛客/test.cpp
@@ -1,79 +1,60 @@
+/*
+@File    :   test.cpp
+@Time    :   2020/04/20 09:46:06
+@Author  :   Zhang tao 
+@Version :   1.0
+@Desc    :   test.cpp
+*/
+/*
+*/
 #include <iostream>
-#include <string>
 using namespace std;
-// string strAdd(string a, string b)
-// {
-//   int carry = 0, i = a.size() - 1, j = b.size() - 1;
-//   string ans = "";
-//   while (i >= 0 || j >= 0)
-//   {
-//     int p1 = i >= 0 ? a[i--] - '0' : 0, p2 = j >= 0 ? b[j--] - '0' : 0;
-//     int v = (p1 + p2 + carry);
-//     carry =v/ 10, ans = to_string(v % 10) + ans;
-//   }
-//   return carry == 1 ? "1" + ans : ans;
-// }
-
-string strAdd(string a, string b)
+int clumsy(int N)
 {
-  int carry = 0, i = a.size() - 1, j = b.size() - 1, p1 = 0, p2 = 0, v = 0;
-  string ans = "";
-  while (i >= 0 || j >= 0)
+  int *q1 = new int[N], l1 = 0;
+  for (int i = N; i >= 1; i -= 4)
   {
-    p1 = (i >= 0) ? a[i--] - '0' : 0, p2 = (j >= 0) ? b[j--] - '0' : 0;
-    v = (p1 + p2 + carry);
-    carry = v / 10, ans = to_string(v % 10) + ans;
+    int c = i - 1, y = i;
+    y *= c >= 1 ? c, c-- : 1;
+    y /= c >= 1 ? c : 1;
+    q1[l1++] = y;
+    cout << y << " ";
   }
-  return carry == 1 ? "1" + ans : ans;
-}
-
-
-/***
- * #include <iostream>
-#include <string>
-#include <vector>
-using namespace std;
-string strAdd(string a, string b)
-{
-  int carry = 0, i = a.size() - 1, j = b.size() - 1, p1 = 0, p2 = 0, v = 0;
-  string ans = "";
-  while (i >= 0 || j >= 0)
+  cout << "**" << endl;
+  int ans = 0, c = q1[l1 - 1], j = 0;
+  int v1 = j < l1 ? q1[j++] : 0;
+  for (int i = N - 3; i >= 1; i -= 4)
   {
-    if (i<0){
-        ans=to_string(b[j--]-'0'+carry)+ans,carry=0;
-    }
-     else  if (j<0){
-        ans=to_string(a[i--]-'0'+carry)+ans,carry=0;
-    }else{
-         int v=a[i--]-'0'+b[j--]-'0'+carry;
-         carry=v/10,ans=to_string(v%10)+ans;
-     }
+    int v2 = j < l1 ? q1[j++] : 0;
+    ans = v1 + i - v2;
+    v1 = ans;
+    cout << v1 << " " << i << " " << v2 << " " << ans << "% ";
   }
-  return carry == 1 ? "1" + ans : ans;
+  return N - 3 >= 1 ? ans : c;
 }
-int main(){
-    int n;
-    cin>>n;
-    string *dp=new string[n+1];
-    dp[0]="1",dp[1]="1",dp[2]="2";
-    for (int t=3;t<n+1;t++){
-        dp[t]=strAdd(dp[t-1],dp[t-2]);
-     
-    }
-    cout<<dp[n];
-}
- * /
 int main()
 {
-  // int n;
-  // cin >> n;
-  // string *dp = new string[n + 1];
-  // dp[0] = "0", dp[1] = "1", dp[2] = "2";
-  // for (int t = 3; t < n + 1; t++)
-  // {
-  //   dp[t] = strAdd(dp[t - 1], dp[t - 2]);
-  //   cout << "# " << dp[t] << endl;
-  // }
-  cout << strAdd("123123214321432423423", "783442131312312312312323122") << endl;
-  // cout << dp[n];
+  cout << clumsy(10);
+  return 0;
 }
+
+int clumsy(int N) {
+        int ans = 0;
+        int temp = N;
+        for(int i = 1 ; i < N ; ++ i){
+            int cur = N - i;
+            int type = i % 4;
+            if(type == 0){
+                ans += temp;
+                temp = - cur;
+            }else if(type == 1){
+                temp *= cur;
+            }else if(type == 2){
+                temp /= cur;
+            }else{
+                temp += cur;
+            }
+        }
+        ans += temp;
+        return ans;
+    }
