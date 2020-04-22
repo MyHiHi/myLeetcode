aa2fcd33562b5544c08be3eb49066fde28bd8192
MyHiHi <909535692@qq.com>
2020-04-22 11:42:25 +0800

    560. 和为K的子数组.cpp

--- c++/test.cpp
+++ c++/test.cpp
@@ -1,27 +1,16 @@
 #include <iostream>
-#include <vector>
-#include <map>
+#include <algorithm>
+#include <string>
+
 using namespace std;
 int main()
 {
-  map<int, int> candy;
-  int n = 0;
-  int t;
-  while (cin >> t)
-  {
-    candy[t]++;
-    cout << "**" << candy[t] << endl;
-  }
-
-  for (auto c : candy)
-  {
-    int a = c.first, b = c.second;
-    int people = a + 1, num = b / people, f = b % people;
-    if (f != 0)
-      num++;
-    n += people * num;
-  }
-  cout << n;
-
-  return 0;
+  string p = ":asdfefe";
+  if (p.find('t') == -1)
+    cout << "No";
+  else if (p.find('t') == -1)
+    cout << "No1";
+  cout << "end";
+  p[0] = "1";
+  cout << p;
 }
--- python/test1.py
+++ python/test1.py
@@ -1,2 +1,15 @@
-p = {12: [12, 4]}
-print(p)
+def subArray(nums, k):
+    cnt, sum = 0, 0
+    mp = {0: 1}
+    for i in nums:
+        sum += i
+        print("sum: ", sum)
+        if mp.get(sum-k):
+            print("cnt: ", mp.get(sum-k))
+            cnt += mp.get(sum-k)
+        mp[sum] = mp.get(sum, 0)+1
+        print("mp: ", mp)
+    return cnt
+
+
+print(subArray([1, 1, -1, 1], 2))
