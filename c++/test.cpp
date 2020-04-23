a986dac16b02aad8412836e07f172124038876f2
MyHiHi <909535692@qq.com>
2020-04-23 11:59:45 +0800

    完全背包-组成硬币的个数.cpp

--- c++/test.cpp
+++ c++/test.cpp
@@ -1,16 +1,25 @@
 #include <iostream>
 #include <algorithm>
-#include <string>
+#include <vector>
 
 using namespace std;
+int countWays(int x, int y)
+{
+  vector<vector<int>> dp(x, vector<int>(y));
+  dp[0][0] = 1;
+  for (int i = 0; i < x; i++)
+    for (int j = 0; j < y; j++)
+    {
+      if (i == 0 && j != 0)
+        dp[i][j] = 1;
+      else if (j == 0 && i != 0)
+        dp[i][j] = 1;
+      else
+        dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+    }
+  return dp[x - 1][y - 1];
+}
 int main()
 {
-  string p = ":asdfefe";
-  if (p.find('t') == -1)
-    cout << "No";
-  else if (p.find('t') == -1)
-    cout << "No1";
-  cout << "end";
-  p[0] = "1";
-  cout << p;
+  cout << countWays(2, 2);
 }
